379ce2ac1e39eba7197a8b36c657d12e8fb8e40f
kesici <ibrahim.kesici@outlook.com>
2022-12-05 11:16:43 +0100

    Rerun

--- get_response.py
+++ get_response.py
@@ -6,7 +6,8 @@ class ResponseRetrieval:
         self.to_date = to_date
         self.page_number = page_number
         self._url = f"https://www.statstidende.dk/api/messagesearch?d=false&fromDate={self.from_date}T00:00:00&m=14a1d71df21558e5ade0214f90482cdc&messageloguser=&o=40&page={self.page_number}&ps=10&teamsOnly=false&toDate={self.to_date}T00:00:00&userOnly=false"
-
+        #self._url = f"https://www.statstidende.dk/messages?fromDate=2022-09-01T00%3A00%3A00&m=14a1d71df21558e5ade0214f90482cdc&ps=100&toDate=2022-12-05T00%3A00%3A00"
+        
     def get_response(self):
         response = requests.get(self._url, verify=False)
         return response, response.status_code
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os.path
 import logging
 import pandas as pd
 
@@ -7,50 +6,48 @@ from bankruptcy_estates import BankruptcyEstate
 from get_response import ResponseRetrieval
 from datetime import datetime
 
-excel_file_name = "run2.xlsx"
-path_to_excel_file = f"{os.getcwd()}\{excel_file_name}" 
 
-to_date = datetime.today().strftime("%Y-%m-%d")
-# to_date = "2022-03-01"
-from_date = "2021-09-01"
-page_number = 0
+def main():
+    from_date = "2022-09-01"
+    to_date = datetime.today().strftime("%Y-%m-%d")
+    page_number = 0
+    excel_file_name = f"bancrupcy_from{from_date}_to_{to_date}.xlsx"
 
-response = ResponseRetrieval(from_date=from_date, to_date=to_date, page_number=page_number)
-response, status_code =  response.get_response()
-response_in_json = response.json()
-
-# print(response_in_json)
-# print(len(response_in_json["results"]))
-
-retrieved_data = []
-
-while status_code == 200 and len(response_in_json["results"]) != 0:
-    
-    for data in response_in_json["results"]:
-        try:
-            bankruptcy_estates = BankruptcyEstate(message_number=data["messageNumber"],
-                                                bankruptcy_estate=data["title"],
-                                                cvr_no=data["summary"][0]["value"],
-                                                court_district=data["summary"][1]["value"],
-                                                publish_date=data["published"])
-            retrieved_data.append(bankruptcy_estates.get_as_dict())  
-            
-                                       
-        except Exception as e:
-            logging.exception(e)
-            bankruptcy_estates = BankruptcyEstate(bankruptcy_estate=data["title"])
-            retrieved_data.append(bankruptcy_estates.get_as_dict())   
-            break
-
-    page_number+=1
     response = ResponseRetrieval(from_date=from_date, to_date=to_date, page_number=page_number)
     response, status_code =  response.get_response()
     response_in_json = response.json()
-    print(page_number)
-
-df = pd.DataFrame.from_dict(retrieved_data)
-
-with pd.ExcelWriter(excel_file_name, mode="w") as writer:
-    df.to_excel(writer,sheet_name='Sheet1', index=False)
 
 
+    retrieved_data = []
+
+    while status_code == 200 and len(response_in_json["results"]) != 0:
+        
+        for data in response_in_json["results"]:
+            try:
+                bankruptcy_estates = BankruptcyEstate(message_number=data["messageNumber"],
+                                                    bankruptcy_estate=data["title"],
+                                                    cvr_no=data["summary"][0]["value"],
+                                                    court_district=data["summary"][1]["value"],
+                                                    publish_date=data["published"])
+                retrieved_data.append(bankruptcy_estates.get_as_dict())  
+                
+                                        
+            except Exception as e:
+                logging.exception(e)
+                bankruptcy_estates = BankruptcyEstate(bankruptcy_estate=data["title"])
+                retrieved_data.append(bankruptcy_estates.get_as_dict())   
+                break
+
+        page_number+=1
+        response = ResponseRetrieval(from_date=from_date, to_date=to_date, page_number=page_number)
+        response, status_code =  response.get_response()
+        response_in_json = response.json()
+        print(page_number)
+
+    df = pd.DataFrame.from_dict(retrieved_data)
+
+    with pd.ExcelWriter(excel_file_name, mode="w") as writer:
+        df.to_excel(writer,sheet_name='Sheet1', index=False)
+
+if __name__ == "__main__":
+    main()
